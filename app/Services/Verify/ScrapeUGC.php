6cd30f26b66ba8d98a79119bf85c01c670612578
Jacky Liang <bluelegopiece@hotmail.com>
2015-06-20 01:27:21 -0400

    Re-added Verify

---
+++ app/Services/Verify/ScrapeUGC.php
@@ -0,0 +1,194 @@
+<?php
+namespace App\Services\Verify;
+
+/**
+ * Scrapes two UGC team pages (denoted as "our team" and "their team") and returns
+ * the name of the team and structured lists of the verified roster of "player
+ * name" and "Steam ID"
+ * Jacky Liang July 18 2015
+ */
+
+class ScrapeUGC {
+    // XPath to get the team abbreviation and name
+    const TEAM_ABB_XPATH = '//*[@id="wrapper"]/section/div/div[1]/div[1]/div/div/h1/text()';
+    const TEAM_NAME_XPATH = '//*[@id="wrapper"]/section/div/div[1]/div[1]/div/div/h1/b';
+
+    // XPath to get the Steam Name and ID
+    const NAME_AND_ID_XPATH = '//*[@id="wrapper"]//div[@class="col-md-12"]//h5/b';
+
+    // Two URLs - our and their teams
+    private $ourTeamURL;
+    private $theirTeamURL;
+
+    // Two HTML objects - our and their team's
+    private $ourTeamHTML;
+    private $theirTeamHTML;
+
+    // Two associative arrays - our and their team's
+    private $ourTeamPlayers = array();
+    private $theirTeamPlayers = array();
+
+    // Two team names - our and their team's
+    private $ourTeamName;
+    private $theirTeamName;
+
+    public function __construct($ourTeamURL, $theirTeamURL) {
+
+        // Silence the DOM errors
+        libxml_use_internal_errors(true);
+
+        // Store the two URLs - ours and theirs
+        $this->ourTeamURL = $ourTeamURL;
+        $this->theirTeamURL = $theirTeamURL;
+
+        // Set HTTP response header to plain text for debugging output
+        header("Content-type: text/plain");
+
+        // Generate our team's HTML file
+        $this->ourTeamHTML = new \DomDocument;
+        $this->ourTeamHTML->loadHTMLFile($this->ourTeamURL);
+
+        // Generate their team's HTML file
+        $this->theirTeamHTML = new \DomDocument;
+        $this->theirTeamHTML->loadHTMLFile($this->theirTeamURL);
+
+        $this->ourTeamPlayers = $this->scrapeUGCTeam($this->ourTeamHTML);
+        $this->theirTeamPlayers = $this->scrapeUGCTeam($this->theirTeamHTML);
+
+        $this->ourTeamName = $this->scrapeUGCTeamName($this->ourTeamHTML);
+        $this->theirTeamName = $this->scrapeUGCTeamName($this->theirTeamHTML);
+    }
+
+    /**
+     * Gets our team's URL
+     * @return string Our Team's URL
+     */
+    public function getOurTeamURL(){
+        return $this->ourTeamURL;
+    }
+
+    /**
+     * Get their team's URL
+     * @return string Their Team's URL
+     */
+    public function getTheirTeamURL(){
+        return $this->theirTeamURL;
+    }
+
+    /**
+     * When supplied a valid UGC team URL, return the team name
+     * @param $url     UGC Team URL
+     * @return string  Team name in the form of 'Team Abbreviation' - 'Team Name'
+     */
+    private function scrapeUGCTeamName($url){
+        $xpath = new \DomXPath($url);
+
+        $teamAbbNode = $xpath->query(ScrapeUGC::TEAM_ABB_XPATH);
+        $teamNameNode = $xpath->query(ScrapeUGC::TEAM_NAME_XPATH);
+
+        $teamAbb = '';
+        $teamName = '';
+
+        // Save the team's abbreviation
+        foreach($teamAbbNode as $node) {
+            $teamAbb = trim($node->nodeValue);
+            break;
+        }
+
+        // Save the team's name
+        foreach($teamNameNode as $node) {
+            $teamName = trim($node->nodeValue);
+            break;
+        }
+
+        return $teamAbb . ' - ' . $teamName;
+    }
+
+    /**
+     * When supplied a valid UGC team URL, return an associative array
+     * of the UGC team of Steam Name and Steam ID
+     * @param $url    UGC Team URL
+     * @return array  Associative array of Steam Name -> Steam ID
+     */
+    private function scrapeUGCTeam($url){
+
+        $xpath = new \DomXPath($url);
+
+        // XPath to get the Steam names and Steam IDs
+
+
+        // Get the Steam names and Steam IDs using the XPath
+        $steamNameAndIDs = $xpath->query(ScrapeUGC::NAME_AND_ID_XPATH);
+
+        // Temporary lists to store the Steam Names and Steam IDs
+        $steamNameList = array();
+        $steamIDList = array();
+
+        foreach ($steamNameAndIDs as $id => $node) {
+            $steamName = "";
+            $steamID = "";
+
+            // Store Steam name first (which is the even-numbered item)
+            // in the scraped list. Then store the Steam ID (which is
+            // the odd-numbered item).
+            if($id % 2 == 0) {
+                $steamName = $node->nodeValue;
+            } else if ($id % 2 == 1) {
+                $steamID = $node->nodeValue;
+            }
+
+            if(!empty($steamName)) {
+                // Add each Steam name onto our team's list
+                array_push($steamNameList, $steamName);
+            } else if(!empty($steamID)){
+                // Add each Steam ID onto our team's list
+                array_push($steamIDList, $steamID);
+            }
+        }
+
+        // Combine Steam ID and Steam Name to team's associative
+        // list
+        $team = array_combine(
+            $steamNameList,
+            $steamIDList
+        );
+
+        return $team;
+    }
+
+    /**
+     * Returns an associative array of our team
+     * where the key is the Steam name and the
+     * value is the Steam ID
+     * @return array Associative array of our team
+     */
+    public function getOurTeam(){
+        return $this->ourTeamPlayers;
+    }
+
+    /**
+     * Returns an associative array of their team
+     * where the key is the Steam name and the
+     * value is the Steam ID
+     * @return array
+     */
+    public function getTheirTeam(){
+        return $this->theirTeamPlayers;
+    }
+
+    /**
+     * Get our team name
+     * @return string Our team name
+     */
+    public function getOurTeamName(){
+        return $this->ourTeamName;
+    }
+
+    /**
+     * Get their team name
+     * @return string Their team name
+     */
+    public function getTheirTeamName(){
+        return $this->theirTeamName;
+    }
+}
---
+++ app/Services/Verify/Verify.php
@@ -0,0 +1,189 @@
+<?php
+namespace App\Services\Verify;
+/**
+ * Verify uses ParseStatus and ScrapeUGC to determine whether players are in the
+ * current server are rostered or not
+ * Jacky Liang June 18 2015
+ */
+require_once("ParseStatus.php");
+require_once("ScrapeUGC.php");
+require_once("SteamID.php");
+
+class Verify {
+    // Steam profile URL
+    const STEAM_PROFILE = 'http://steamcommunity.com/profiles/';
+
+    // Each server contains three different types of players - players from
+    // our team, players from their team, and unrostered players. These private
+    // vars stores these players
+    private $ourTeamRoster = array();
+    private $theirTeamRoster = array();
+    private $unrostered = array();
+
+    // Our team and their team names
+    private $ourTeamName;
+    private $theirTeamName;
+
+    // Our team and their team URLs
+    private $ourTeamURL;
+    private $theirTeamURL;
+
+    // Mapping name to Steam URL
+    private $ourTeamProfile;
+    private $theirTeamProfile;
+    private $unrosteredProfile;
+
+    /**
+     * Determine the roster status of all players within the current server
+     * @param $ourTeamURL   UGC URL of your team
+     * @param $theirTeamURL UGC URL of the opposing team
+     * @param $statusOutput The server output when you type in `status`
+     */
+    public function __construct(
+        $ourTeamURL,
+        $theirTeamURL,
+        $statusOutput
+    ){
+        $scrape = new ScrapeUGC($ourTeamURL, $theirTeamURL);
+        $status = new ParseStatus($statusOutput);
+
+        $serverPlayers = $status->getTeam();
+
+        $ourTeamPlayers = $scrape->getOurTeam();
+        $theirTeamPlayers = $scrape->getTheirTeam();
+
+        // Store our and their team's names into these class vars
+        $this->ourTeamName = $scrape->getOurTeamName();
+        $this->theirTeamName = $scrape->getTheirTeamName();
+
+        // Store our and their team's URLs into these class var
+        $this->ourTeamURL = $scrape->getOurTeamURL();
+        $this->theirTeamURL = $scrape->getTheirTeamURL();
+
+        // Loop through the current server's player list and compare each Steam
+        // ID from the status output to the teams. We're basically taking the
+        // Steam IDs from the status output and comparing each value to
+        // our and their team's roster list. This generates the our team and their
+        // team roster list. If there are no matches, it indicates the individual
+        // is unrostered.
+        foreach($serverPlayers as $name => $steamID) {
+            if(in_array($steamID, $ourTeamPlayers)) {
+                // Add to our team's roster list + profiles
+                $this->ourTeamRoster[$name] = $steamID;
+                $this->ourTeamProfile[$name] = SteamID::parse($steamID)->profileURL();
+            } else if (in_array($steamID, $theirTeamPlayers)) {
+                // Add to their team's roster list + profiles
+                $this->theirTeamRoster[$name] = $steamID;
+                $this->theirTeamProfile[$name] = SteamID::parse($steamID)->profileURL();
+            } else {
+                // Add to unrostered + profiles
+                $this->unrostered[$name] = $steamID;
+                $this->unrosteredProfile[$name] = SteamID::parse($steamID)->profileURL();
+            }
+        }
+    }
+
+    /**
+     * Get our roster's name
+     * @return string Our roster name
+     */
+    public function getOurRosterName(){
+        return $this->ourTeamName;
+    }
+
+    /**
+     * Get their roster's name
+     * @return string Their roster name
+     */
+    public function getTheirRosterName(){
+        return $this->theirTeamName;
+    }
+
+    /**
+     * Get the size of our roster within the server
+     * @return int The size of our roster within the server
+     */
+    public function getOurRosterSize(){
+        return count($this->ourTeamRoster);
+    }
+
+    /**
+     * Get the size of their roster within the server
+     * @return int The size of their roster within the server
+     */
+    public function getTheirRosterSize(){
+        return count($this->theirTeamRoster);
+    }
+
+    /**
+     * Get the number of unrostered people within the server
+     * @return int The number of unrostered people within the server
+     */
+    public function getUnrosteredSize(){
+        return count($this->getUnrostered());
+    }
+
+    /**
+     * Get the URL of our roster
+     * @return string The UGC URL of our roster
+     */
+    public function getOurRosterURL() {
+        return $this->ourTeamURL;
+    }
+
+    /**
+     * Get the URL of their roster
+     * @return string The UGC URL of their roster
+     */
+    public function getTheirRosterURL() {
+        return $this->theirTeamURL;
+    }
+
+    /**
+     * Get all of our team's players within the server
+     * @return array Our team's players within the server
+     */
+    public function getOurRoster() {
+        return $this->ourTeamRoster;
+    }
+
+    /**
+     * Get all of their team's players within the server
+     * @return array Their team's players within the server
+     */
+    public function getTheirRoster() {
+        return $this->theirTeamRoster;
+    }
+
+    /**
+     * Get all unrostered players within the server
+     * @return array All unrostered players within the server
+     */
+    public function getUnrostered() {
+        return $this->unrostered;
+    }
+
+    /**
+     * Get all of our team's profile URLs
+     * @return mixed
+     */
+    public function getOurTeamProfile() {
+        return $this->ourTeamProfile;
+    }
+
+    /**
+     * Get all of their team's profile URLs
+     * @return mixed
+     */
+    public function getTheirTeamProfile() {
+        return $this->theirTeamProfile;
+    }
+
+    /**
+     * Get all unrostered player's profile URLs
+     * @return mixed
+     */
+    public function getUnrosteredProfile() {
+        return $this->unrosteredProfile;
+    }
+}
