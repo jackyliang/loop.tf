fc26efab8d6140404bbd774818593dc24cd54718
Jacky Liang <bluelegopiece@hotmail.com>
2015-06-20 19:25:48 -0400

    task #10: added cl and rn to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,4 +2,8 @@
     <p class="text-muted text-center">
         Made with ♥ by <a href="http://steamcommunity.com/id/looptf2">loop</a>
     </p>
+    <p class="text-muted text-center">
+        Found a <a href="https://github.com/jackyliang/loop.tf/issues/new">bug</a>?  <b>|</b>
+        <a href="https://github.com/jackyliang/loop.tf#release-notes">Release Notes</a>
+    </p>
 </div>
