29e8670af80a6ab7a3919f9daccd3dbc4434c26e
Jacky Liang <bluelegopiece@hotmail.com>
2015-07-24 01:58:51 -0400

    task #28: highlight search results

--- resources/views/js/classes-autocomplete.blade.php
+++ resources/views/js/classes-autocomplete.blade.php
@@ -1,6 +1,15 @@
 <script type="text/javascript">
     $(function()
     {
+        $.ui.autocomplete.prototype._renderItem = function( ul, item){
+            var term = this.term.split(' ').join('|');
+            var re = new RegExp("(" + term + ")", "gi") ;
+            var t = item.label.replace(re,"<b>$1</b>");
+            return $( "<li></li>" )
+                    .data( "item.autocomplete", item )
+                    .append( "<a>" + t + "</a>" )
+                    .appendTo( ul );
+        };
         $( "#q" ).autocomplete({
             source: '{{ URL('autocomplete') }}',
             minLength: 3,
