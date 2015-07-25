90b24eae1bfa1bd771c7d6c449ad3ba785a22f34
Jacky Liang <bluelegopiece@hotmail.com>
2015-07-25 17:31:18 -0400

    task #29: added the table views of class results

--- resources/views/schedulizer/results.blade.php
+++ resources/views/schedulizer/results.blade.php
@@ -6,13 +6,34 @@
 
 @section('content')
     <h1 class="page-heading">Results</h1>
-    <div class="container half-page">
+    <div class="container">
     @if($classesByType)
         @foreach ($classesByType as $type => $classes)
-            <h1>{{ $type }}</h1>
-            @foreach ($classes as $class)
-                <p>{{ $class['subject_code'] }} {{ $class['course_code'] }} ...</p>
-            @endforeach
+            <h3>{{ $type }}</h3>
+            <table class="table table-striped">
+                <th>Class</th>
+                <th>Section</th>
+                <th>Name</th>
+                <th>CRN</th>
+                <th>Day</th>
+                <th>Time</th>
+                <th>Instructor</th>
+                <th>Max Enroll</th>
+                <th>Enroll</th>
+                @foreach ($classes as $class)
+                    <tr>
+                        <td> {{ $class->subject_code . ' ' . $class->course_no}} </td>
+                        <td> {{ $class->section}} </td>
+                        <td> {{ $class->course_title}} </td>
+                        <td> {{ $class->crn}} </td>
+                        <td> {{ $class->day}} </td>
+                        <td> {{ $class->time}} </td>
+                        <td> {{ $class->instructor}} </td>
+                        <td> {{ $class->max_enroll}} </td>
+                        <td> {{ $class->enroll}} </td>
+                    </tr>
+                @endforeach
+            </table>
         @endforeach
     @endif
     </div>
