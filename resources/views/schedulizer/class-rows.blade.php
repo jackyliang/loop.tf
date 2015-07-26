aa35b4a557dd6deedb2f4b6a59026a20e5646fd2
Jacky Liang <bluelegopiece@hotmail.com>
2015-07-25 23:52:42 -0400

    close #31: added additional links to professors and courses

--- resources/views/schedulizer/class-rows.blade.php
+++ resources/views/schedulizer/class-rows.blade.php
@@ -1,10 +1,20 @@
-<td> {{ $class->subject_code . ' ' . $class->course_no}} </td>
+<td><a
+        href="{{ URL('schedulizer/results?q=' . $class->subject_code . ' ' . $class->course_no) }}"
+    >
+        {{ $class->subject_code . ' ' . $class->course_no}}
+    </a>
+</td>
 <td> {{ $class->section}} </td>
 <td> {{ $class->crn}} </td>
 <td> {{ $class->course_title}} </td>
 <td> {{ $class->day}} </td>
 <td> {{ $class->time}} </td>
-<td> {{ $class->instructor}} </td>
+<td><a
+        href="{{ URL('schedulizer/results?q=' . $class->instructor) }}"
+    >
+        {{ $class->instructor}}
+    </a>
+</td>
 <td> {{ $class->enroll}} </td>
 <td> {{ $class->max_enroll}} </td>
 <td> {{ $class->credits}} </td>
