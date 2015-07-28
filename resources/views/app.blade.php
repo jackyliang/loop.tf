9f8bf4b3e2a7e3c52e8e2b5726c7dd4ffb12c253
Jacky Liang <bluelegopiece@hotmail.com>
2015-07-28 01:14:00 -0400

    test material design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,17 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title')</title>
 
-    <!-- CSS -->
-	<link href="/css/all.css" rel="stylesheet">
-    <!-- Scripts -->
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-    <script src='http://code.jquery.com/ui/1.10.2/jquery-ui.js'></script>
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js'></script>
-    <script type="text/javascript"></script>
-
-	<!-- Fonts -->
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
+    @include('libs.libs')
     @include('google.analytics')
 
 </head>
@@ -32,3 +22,9 @@
     </div>
 </body>
 </html>
+
+<script>
+    $(document).ready(function() {
+        $.material.init();
+    });
+</script>
--- resources/views/schedulizer/search.blade.php
+++ resources/views/schedulizer/search.blade.php
@@ -10,8 +10,6 @@
         <h1>Search for a Class</h1>
     </div>
 
-
-    </form>
     <form
         method="GET"
         action="{{ URL('schedulizer/results') }}"
@@ -20,7 +18,7 @@
         <div class="form-group">
             <input type="search" class="form-control" id="q" name="q" placeholder="i.e. ECE 201, Digital Logic, Kandasamy, or 41045">
         </div>
-        <button type="submit" id="s" class="btn btn-success">
+        <button type="submit" id="s" class="btn btn-material-teal">
             <span class="glyphicon glyphicon-search"></span>
         </button>
     </form>
