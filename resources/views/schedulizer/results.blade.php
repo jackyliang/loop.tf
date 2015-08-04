aaf5c0bea6fd3b65c2587ee920987cc57f1308ec
Jacky Liang <bluelegopiece@hotmail.com>
2015-08-04 14:08:41 -0400

    task 40: added add to cart button and fixed hierarchy of text

--- resources/views/schedulizer/results.blade.php
+++ resources/views/schedulizer/results.blade.php
@@ -14,38 +14,51 @@
     @if($classesByLabelAndType)
         <?php $i = 1; ?>
         @foreach ($classesByLabelAndType as $label => $classesByType)
-            <h3>{{ $label }}</h3>
+            <h3
+                style="margin: 50px 0px 0px 0px;">
+                {{ $label }}
+            </h3>
             @foreach ($classesByType as $type => $classes)
-                <h4>{{ $type }}</h4>
-                <table class="table table-striped">
-                    <th>Class</th>
-                    <th>Section</th>
-                    <th>CRN</th>
-                    <th>Day</th>
-                    <th>Time</th>
-                    <th>Professor</th>
-                    <th>Enroll</th>
-                    <th>Max Enroll</th>
-                    <th>Credits</th>
-                    @foreach ($classes as $class)
-                        {{-- Highlight class with grey if they're full --}}
-                        @if(
-                            $class->enroll === $class->max_enroll ||
-                            $class->enroll === "CLOSED"
-                        )
-                            <tr data-toggle="collapse" data-target="#class{{ $i }}" class="accordion-toggle moreMuted">
-                                @include('schedulizer.class-rows')
-                            </tr>
-                        @else
-                            <tr data-toggle="collapse" data-target="#class{{ $i }}" class="accordian-toggle info">
-                                @include('schedulizer.class-rows')
-                            </tr>
-                        @endif
-                        <?php $i++; ?>
-                    @endforeach
-                </table>
+                <div style="margin: 0px 0px 0px 30px;">
+                    <h4>{{ $type }}
+                        <a
+                            href="javascript:void(0)"
+                            class="btn btn-primary btn-xs btn-raised mdi-content-add"
+                            style="margin: 10px 20px;">
+                            Add This!
+                        </a>
+                    </h4>
+                    <table class="table table-striped">
+                        <th>Class</th>
+                        <th>Section</th>
+                        <th>CRN</th>
+                        <th>Day</th>
+                        <th>Time</th>
+                        <th>Professor</th>
+                        <th>Enroll</th>
+                        <th>Max Enroll</th>
+                        <th>Credits</th>
+                        @foreach ($classes as $class)
+                            {{-- Highlight class with grey if they're full --}}
+                            @if(
+                                $class->enroll === $class->max_enroll ||
+                                $class->enroll === "CLOSED"
+                            )
+                                <tr data-toggle="collapse" data-target="#class{{ $i }}" class="accordion-toggle moreMuted">
+                                    @include('schedulizer.class-rows')
+                                </tr>
+                            @else
+                                <tr data-toggle="collapse" data-target="#class{{ $i }}" class="accordian-toggle info">
+                                    @include('schedulizer.class-rows')
+                                </tr>
+                            @endif
+                            <?php $i++; ?>
+                        @endforeach
+                    </table>
+                </div>
             @endforeach
         @endforeach
     @endif
     </div>
+    <button class="btn btn-fab btn-raised btn-material-red"><i class="mdi-navigation-arrow-forward"></i></button>
 @stop
