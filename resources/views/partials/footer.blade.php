fd101fd0ccd4eebc5fa83e041d83270f38f81cd9
Jacky Liang <bluelegopiece@hotmail.com>
2015-08-13 21:41:59 -0400

    you guessed it. footer update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,8 +4,8 @@
     </p>
     <h6 class="text-muted text-center">
         <small>
-            <a href="https://github.com/jackyliang/loop.tf/issues/new">Report a Bug</a>  |
-            <a href="https://github.com/jackyliang/loop.tf#release-notes">Release Notes</a>  |
+            {{--<a href="https://github.com/jackyliang/loop.tf/issues/new">Report a Bug</a>  |--}}
+            {{--<a href="https://github.com/jackyliang/loop.tf#release-notes">Release Notes</a>  |--}}
             <a href="http://steamcommunity.com/groups/loop-tf">Steam Group</a>
         </small>
     </h6>
