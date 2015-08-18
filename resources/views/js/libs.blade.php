235a48b2beb81ceede46d1fdc555501cc17f44bb
Jacky Liang <bluelegopiece@hotmail.com>
2015-08-18 17:15:49 -0400

    task #47: added fullcalendar libs

--- resources/views/js/libs.blade.php
+++ resources/views/js/libs.blade.php
@@ -4,6 +4,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/2.0.0/pnotify.all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.4.0/fullcalendar.min.js"></script>
+
 <script>
     $(function() {
         $.material.init();
