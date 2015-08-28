0be6f45c5df731eb66c27e1c89004ada89443cf6
Jacky Liang <bluelegopiece@hotmail.com>
2015-08-28 00:03:32 -0400

    master: footer glyphs

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,6 +9,6 @@
         <h1>Welcome to loop.tf!</h1>
         <p>This is where I host my personal projects. Feel free to check out my other stuff on <a href="https://github.com/jackyliang">Github</a>!</p>
     </div>
+    @include('js.header-padding')
 @stop
 
-@include('js.header-padding')
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -12,8 +12,8 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="{{ Request::is('ugc') ? 'active' : '' }}"><a href="{{ url('/ugc') }}">Verify UGC</a></li>
-                <li class="{{ Request::is('schedulizer') ? 'active' : '' }}"><a href="{{ url('/schedulizer/search') }}">Schedulizer</a></li>
+                <li class="{{ Request::is('ugc') ? 'active' : '' }}"><a href="{{ url('/ugc') }}"><span class="glyphicon glyphicon-ok"></span> Verify UGC</a></li>
+                <li class="{{ Request::is('schedulizer') ? 'active' : '' }}"><a href="{{ url('/schedulizer/search') }}"><span class="glyphicon glyphicon-education"></span> Schedulizer</a></li>
             </ul>
         </div>
 
